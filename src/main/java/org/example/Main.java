3826480e948f26e7f375965deb83c433e71d3acd
Evgeniy <gerasum04@gmail.com>
2022-05-31 23:23:00 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+/*
+Напишите программу, которая меняет местами элементы одномерного массива из String в обратном порядке.
+Не используйте дополнительный массив для хранения результатов.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
